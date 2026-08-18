9571b19fa952c6a61ccbc74dfe8a19dfc86f178e
Quan Le <lequan170205@gmail.com>
2026-08-19 00:10:53 +0800

    perf: preserve FlashList message bubble recycling

--- src/components/chat/MessageBubble.tsx
+++ src/components/chat/MessageBubble.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { getMessageBubbleRecyclingKey } from '../../lib/messageBubbleRecycling'
+
 import { MessageBubble as MemoizedMessageBubble } from './MessageBubbleImpl'
 
 export { VALID_EMOJIS } from './MessageBubbleImpl'
@@ -7,21 +9,11 @@ export type { MessageBubbleContextMenuPayload } from './MessageBubbleImpl'
 
 type MessageBubbleProps = React.ComponentProps<typeof MemoizedMessageBubble>
 
-const getCitationRenderKey = (props: MessageBubbleProps) =>
-  JSON.stringify(
-    (props.message.metadata?.citations ?? []).map((citation) => ({
-      sourceType: citation.sourceType,
-      reelId: citation.reelId,
-      evidenceType: citation.evidenceType,
-      title: citation.title,
-      startTime: citation.startTime,
-      endTime: citation.endTime,
-      quote: citation.quote,
-    })),
-  )
-
 export function MessageBubble(props: MessageBubbleProps) {
   return (
-    <MemoizedMessageBubble key={`${props.message.id}:${getCitationRenderKey(props)}`} {...props} />
+    <MemoizedMessageBubble
+      key={getMessageBubbleRecyclingKey(props.message.metadata?.citations)}
+      {...props}
+    />
   )
 }
