cf2b532e8dad3c1d949ba194c28c690db979d048
lequan170205 <lequan170205@gmail.com>
2026-09-16 22:36:40 +0700

    ci(ios): normalize cloud pod support paths

--- ios/Pods/Target Support Files/simdjson/simdjson-umbrella.h
+++ ios/Pods/Target Support Files/simdjson/simdjson-umbrella.h
@@ -10,7 +10,7 @@
 #endif
 #endif
 
-#include "../../../../node_modules/.pnpm/@nozbe+simdjson@3.9.4/node_modules/@nozbe/simdjson/src/simdjson.h"
+#include "../../../../node_modules/@nozbe/simdjson/src/simdjson.h"
 
 FOUNDATION_EXPORT double simdjsonVersionNumber;
 FOUNDATION_EXPORT const unsigned char simdjsonVersionString[];
