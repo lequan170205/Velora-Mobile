c303860113a2c2ffa9088e29ebe5eb50b5f90a7b
lequan170205 <lequan170205@gmail.com>
2026-09-16 20:15:54 +0700

    ci(ios): include CocoaPods build support files

---
+++ ios/Pods/Target Support Files/ExpoVideoCache/ExpoVideoCache-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ExpoVideoCache : NSObject
+@end
+@implementation PodsDummy_ExpoVideoCache
+@end
---
+++ ios/Pods/Target Support Files/VeloraSystemCalls/VeloraSystemCalls-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VeloraSystemCalls : NSObject
+@end
+@implementation PodsDummy_VeloraSystemCalls
+@end
